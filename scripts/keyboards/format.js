ff129ce3257ac58e235542e057f1a309a638ab46
Nick Wong <hrpearl01@gmail.com>
2013-12-28 23:53:00 +0000

    add grouping
    not fully implemented yet

--- scripts/keyboards/format.js
+++ scripts/keyboards/format.js
@@ -9,3 +9,29 @@
 /* Format
 ------------------------*/ 
 
+window.onload= function(){
+	document.getElementsByClassName('gactive')[0].onclick = function () {
+		var chs = document.querySelectorAll(".hidden"),
+			ul = document.getElementsByClassName("align")[0];
+		if(chs[0] !== undefined){
+			for (var i=0; i<chs.length; i++) {
+				chs[i] = chs[i].classList.remove("hidden");
+				chs[i] = chs[i].classList.add("active");
+			}
+			var li = document.createElement("li"), 
+				img = document.createElement("img");
+			img.src = "style/icons/misc/garrow.svg";
+			img.classList.add("arrow");
+			li.appendChild(img);
+			ul.insertBefore(li, chs[0].parentNode);
+		} else {
+			chs = document.querySelectorAll(".active");
+			for (var i=0; i<chs.length; i++) {
+				chs[i] = chs[i].classList.remove("active");
+				chs[i] = chs[i].classList.add("hidden");
+			}
+			var li = document.getElementsByClassName("arrow").parentNode;
+			ul.removeChild(li);
+		}
+	}
+}
